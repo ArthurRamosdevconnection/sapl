a90cd6eff405c2374df0fb40f78e2a6f19d6803a
Arthur Ramos <arthramos567@gmail.com>
2025-09-02 14:30:57 -0300

    path do templates

--- sapl/settings.py
+++ sapl/settings.py
@@ -313,7 +313,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = PROJECT_DIR.child("collected_static")
 
 STATICFILES_DIRS = (
-    BASE_DIR.child('static'), BASE_DIR.child('sapl').child('pwa').child('templates'),
+    BASE_DIR.child('static'), PROJECT_DIR.child('pwa').child('templates'),
 )
 if FRONTEND_CUSTOM:
     STATICFILES_DIRS = (
